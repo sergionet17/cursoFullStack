17db1d4f607f2ab9680907eb79ffc5b55f066493
alex13mail <alex13mail@yahoo.com>
2018-06-30 16:42:54 -0500

    x[i]
    x[i]

--- micarpeta/js/hola_2.js
+++ micarpeta/js/hola_2.js
@@ -1,3 +1,6 @@
+var variableGlobal = 0;
+
+
 function operaciones3 (precio,iva) {
 	var precio =9;
 	var iva =10;
@@ -5,10 +8,31 @@ function operaciones3 (precio,iva) {
 	return  valor;
 }
 
+function enviarMensaje(){
+	var mensaje = "HOLA MUNDO";
+	return mensaje;
+}
+
 function operaci_llama_funcion (){
 	
 	var valorFuncion = operaciones3(12,12.3);
 	alert (valorFuncion);
-	
+	var mensaje = enviarMensaje();
+    alert (mensaje);
 }
 
+function operacion5(){
+	var mensaje = "eSTO ES UN MENSAJE DE PRUEBA PARA PROBAR LÑAS FUNCIONES ESTO ES UN MENSAJE DE PRUEBA PARA PROBAR LAS FUNCIONES";
+	var mensaje_con_split = mensaje.split("");
+	var resultado="";
+	
+	for (i in mensaje_con_split){
+		if(mensaje_con_split[i] == 'A'){
+			//break;
+			continue;
+		}else {
+			resultado = resultado + mensaje_con_split[i];
+		}
+	}
+	alert(resultado);
+}
